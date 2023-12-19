1ae9172b455625919b9cf4ae0429981981d38162
ilgzmz <ilgzmz@gmail.com>
2023-12-19 12:00:56 -0600

    Update of Eureka server

--- user-svc/src/main/java/com/user/svc/UserSvcApplication.java
+++ user-svc/src/main/java/com/user/svc/UserSvcApplication.java
@@ -2,7 +2,6 @@ package com.user.svc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /*
@@ -10,7 +9,6 @@ Sets this class as a Feign Client
 */
 @EnableFeignClients
 @SpringBootApplication
-@EnableDiscoveryClient
 public class UserSvcApplication {
 
 	public static void main(String[] args) {
